304512b4803ae694a4cecdfbf8d16b2d74b8758a
HeartLezz7 <heartchantapath@gmail.com>
2023-12-18 18:25:42 +0700

    finish get all user id controller

--- src/app/app.go
+++ src/app/app.go
@@ -5,13 +5,13 @@ import (
 	"go-api/src/model"
 	"go-api/src/router"
 
-	// "database/sql"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
 	port := ":8080"
+
 	r := model.Server{Engine: engine}
 	router.UserRouter(r, "/user")
 	fmt.Println("server run on port ", port)
